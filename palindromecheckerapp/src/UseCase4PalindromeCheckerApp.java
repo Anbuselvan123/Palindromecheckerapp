223155b868e1bcb46e0af425a6b5f49401773bdb
Anbuselvan123 <sanbu5199@gmail.com>
2026-03-18 09:57:32 +0530

    UC4: Character array palindrome using two-pointer

---
+++ palindromecheckerapp/src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Input string
+        String input = "radar";
+
+        // Convert string to character array
+        char[] chars = input.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
